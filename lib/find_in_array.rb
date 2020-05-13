5ca4f42f7a28572aad90c29b14b34b0904a235d4
David Lee <david.lee.sl2634@yale.edu>
2020-05-13 19:37:15 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,10 @@
 def find_element_index(array, value_to_find)
   counter = 0 
+  index_value = nil
   while counter < array.length do
+    if array[counter] == value_to_find
+      index_value = count 
+    end
     puts array[counter] == value_to_find
     counter += 1 
   end
